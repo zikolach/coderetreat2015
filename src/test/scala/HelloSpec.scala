2d97eb53c962df08731240c277cc257f99d94b76
Nikolay Kushin <nikolay@indoo.rs>
2015-11-13 23:42:05 +0100

    updated

--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -2,6 +2,9 @@ import org.scalatest._
 
 class HelloSpec extends FlatSpec with Matchers {
   "Hello" should "have tests" in {
-    true should === (true)
+    true should be (true)
+  }
+  it should "add numbers" in {
+    1 + 1 should be (2)
   }
 }
