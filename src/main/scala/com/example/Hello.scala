b4cd65aa907860c5eb9129ea1f0740f75ec81239
Nikolay Kushin <nikolay@indoo.rs>
2015-11-14 12:34:36 +0100

    working

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -4,4 +4,57 @@ object Hello {
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
   }
+
+  case class Cell(x: Int, y: Int)
+
+  var generation = Set.empty[Cell]
+
+  def evolve(): Unit = {
+    var nextGeneration: Set[Cell] = Set.empty
+
+    generation.foreach(cell => {
+      nextGeneration += cell
+      nextGeneration += Cell(cell.x - 1, cell.y - 1)
+      nextGeneration += Cell(cell.x, cell.y - 1)
+      nextGeneration += Cell(cell.x + 1, cell.y - 1)
+      nextGeneration += Cell(cell.x - 1, cell.y)
+      nextGeneration += Cell(cell.x + 1, cell.y)
+      nextGeneration += Cell(cell.x - 1, cell.y + 1)
+      nextGeneration += Cell(cell.x, cell.y + 1)
+      nextGeneration += Cell(cell.x + 1, cell.y + 1)
+    })
+
+    nextGeneration.foreach(cell => {
+      var cnt = 0
+      cnt += generation.count(_ == Cell(cell.x - 1, cell.y - 1))
+      cnt += generation.count(_ == Cell(cell.x, cell.y - 1))
+      cnt += generation.count(_ == Cell(cell.x + 1, cell.y - 1))
+      cnt += generation.count(_ == Cell(cell.x - 1, cell.y))
+      cnt += generation.count(_ == Cell(cell.x + 1, cell.y))
+      cnt += generation.count(_ == Cell(cell.x - 1, cell.y + 1))
+      cnt += generation.count(_ == Cell(cell.x, cell.y + 1))
+      cnt += generation.count(_ == Cell(cell.x + 1, cell.y + 1))
+
+      if (cnt < 2) {
+        nextGeneration -= cell
+      } else if(cnt > 3) {
+        nextGeneration -= cell
+      }
+    })
+
+    generation = nextGeneration
+  }
+
+  var space: String = ""
+
+  def print(): Unit = {
+
+    if (generation.nonEmpty) {
+      space = "x"
+    }
+
+
+//    println(space)
+  }
+
 }
--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -1,10 +1,54 @@
+import com.example.Hello
+import com.example.Hello.Cell
 import org.scalatest._
 
 class HelloSpec extends FlatSpec with Matchers {
-  "Hello" should "have tests" in {
-    true should be (true)
+  "Cell" should "have tests" in {
+    true should be(true)
   }
   it should "add numbers" in {
-    1 + 1 should be (2)
+    1 + 1 should be(2)
+  }
+
+  it should "have no cells in an empty generation" in {
+    Hello.generation = Set.empty
+    Hello.evolve()
+    Hello.generation.size should be(0)
+  }
+
+  it should "kill a cell when under populated" in {
+    Hello.generation = Set(Cell(1, 1), Cell(1, 2))
+    Hello.evolve()
+    Hello.generation.contains(Cell(1, 1)) should be(false)
+  }
+
+  it should "survive when has two neighbours" in {
+    Hello.generation = Set(Cell(1, 1), Cell(1, 2), Cell(2, 1))
+    Hello.evolve()
+    Hello.generation.contains(Cell(1, 1)) should be(true)
+  }
+
+  it should "die when neighbours overcrowded" in {
+    Hello.generation = Set(Cell(1, 1), Cell(1, 2), Cell(2, 1), Cell(2, 2), Cell(1, 0))
+    Hello.evolve()
+    Hello.generation.contains(Cell(1, 1)) should be(false)
+  }
+
+  it should "be born if has exact 3 neighbours" in {
+    Hello.generation = Set(Cell(1, 2), Cell(2, 3), Cell(3, 3))
+    Hello.evolve()
+    Hello.generation.contains(Cell(2, 2)) should be(true)
+  }
+
+  "Space" should "be empty for no cells" in {
+    Hello.generation = Set.empty
+    Hello.print()
+    Hello.space should be ("")
+  }
+
+  it should "print one cell" in {
+    Hello.generation = Set(Cell(1,4))
+    Hello.print()
+    Hello.space should be ("x")
   }
 }
