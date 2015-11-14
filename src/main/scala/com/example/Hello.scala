efb60645f53aeb0b5ca20154e618136418e4566e
Nikolay Kushin <nikolay@indoo.rs>
2015-11-14 16:35:39 +0100

    aaa

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -5,22 +5,14 @@ object Hello {
     println("Hello, world!")
   }
 
+  val range = -1 to 1
+
   def evolve(gen: (Int, Int) => Boolean): (Int, Int) => Boolean = {
-
-    (x: Int, y: Int) => {
-
-      val neighbourStates = for {
-        i <- -1 to 1
-        j <- -1 to 1
-        if !(i == 0 && j == 0)
-      } yield gen(i, j)
-
-      val livingCnt = neighbourStates.count(state => state)
-
-      livingCnt match {
-        case 2 | 3 => true
-        case _ => false
-      }
+    (x: Int, y: Int) => range.flatMap(i => range.map(j => (i, j)))
+      .filter(p => !(p._1 == 0 && p._2 == 0))
+      .count(p => gen(p._1, p._2)) match {
+      case 2 | 3 => true
+      case _ => false
     }
   }
 }
