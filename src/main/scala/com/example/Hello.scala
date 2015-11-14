f9290121c713e856dd86bb4c7e2dbc337837767c
Nikolay Kushin <nikolay@indoo.rs>
2015-11-14 11:28:12 +0100

    working

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -4,4 +4,40 @@ object Hello {
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
   }
+
+  case class Cell(x: Int, y: Int)
+
+  def evolve(gen: Set[Cell]): Set[Cell] = {
+
+    def cellEvolve(cell: Cell): Option[Cell] = {
+      val ifCellAlive = gen.contains(cell)
+
+      val aliveNeighbours = gen.intersect(neighbours(cell))
+      if (aliveNeighbours.size == 2 || aliveNeighbours.size == 3) {
+        Some(cell)
+      } else {
+        None
+      }
+    }
+
+    val cellsToCheck = gen.flatMap(cell => neighbours(cell) + cell)
+
+    val space = cellsToCheck.map { case cell: Cell => cellEvolve(cell) }
+
+    space.filter(_.nonEmpty).map(_.get)
+  }
+
+  def neighbours(cell: Cell): Set[Cell] = {
+    Set(
+      Cell(cell.x - 1, cell.y - 1),
+      Cell(cell.x, cell.y - 1),
+      Cell(cell.x + 1, cell.y - 1),
+      Cell(cell.x - 1, cell.y),
+      Cell(cell.x + 1, cell.y),
+      Cell(cell.x - 1, cell.y + 1),
+      Cell(cell.x, cell.y + 1),
+      Cell(cell.x + 1, cell.y + 1)
+    )
+  }
+
 }
--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -1,10 +1,47 @@
+import com.example.Hello
+import com.example.Hello.Cell
 import org.scalatest._
 
 class HelloSpec extends FlatSpec with Matchers {
-  "Hello" should "have tests" in {
-    true should be (true)
+  "Game of Life" should "have tests" in {
+    true should be(true)
   }
   it should "add numbers" in {
-    1 + 1 should be (2)
+    1 + 1 should be(2)
   }
+  it should "no living cell if set is empty" in {
+    Hello.evolve(Set.empty) should be(Set.empty)
+  }
+
+  it should "return empty set if only one living cell" in {
+    Hello.evolve(Set {
+      Cell(1, 1)
+    }) should be(Set.empty)
+  }
+  it should "stay alive with 2 active neighbours" in {
+    val nextGen = Hello.evolve(Set(Cell(1, 1), Cell(0, 1), Cell(1, 2), Cell(2, 2)))
+    nextGen.contains(Cell(1, 1)) should be(true)
+  }
+
+  it should "born with exact 3 active neighbours" in {
+    val nextGen = Hello.evolve(Set(Cell(0, 1), Cell(1, 2), Cell(2, 2)))
+    nextGen.contains(Cell(1, 1)) should be(true)
+  }
+
+  it should "die when cell has more than 3 neighbours" in {
+
+    val nextGen = Hello.evolve(Set(
+      Cell(0, 0),
+      Cell(1, 0),
+      Cell(2, 0),
+      Cell(0, 1),
+      Cell(1, 1),
+      Cell(2, 1),
+      Cell(0, 2),
+      Cell(1, 2),
+      Cell(2, 2)
+    ))
+      nextGen.contains(Cell(1, 1)) should be(false)
+  }
+
 }
