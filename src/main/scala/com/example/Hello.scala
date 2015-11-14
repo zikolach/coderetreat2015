a4425c3dcc53613b9cd0dc760b47d47fbf8e5806
Nikolay Kushin <nikolay@indoo.rs>
2015-11-14 16:29:33 +0100

    crazy

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -4,4 +4,23 @@ object Hello {
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
   }
+
+  def evolve(gen: (Int, Int) => Boolean): (Int, Int) => Boolean = {
+
+    (x: Int, y: Int) => {
+
+      val neighbourStates = for {
+        i <- -1 to 1
+        j <- -1 to 1
+        if !(i == 0 && j == 0)
+      } yield gen(i, j)
+
+      val livingCnt = neighbourStates.count(state => state)
+
+      livingCnt match {
+        case 2 | 3 => true
+        case _ => false
+      }
+    }
+  }
 }
--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -1,10 +1,55 @@
+import com.example.Hello
 import org.scalatest._
 
 class HelloSpec extends FlatSpec with Matchers {
   "Hello" should "have tests" in {
-    true should be (true)
+    true should be(true)
   }
+
+
   it should "add numbers" in {
-    1 + 1 should be (2)
+    1 + 1 should be(2)
+  }
+
+  "empty space" should "stays empty" in {
+    val emptySpace = (x: Int, y: Int) => false
+    val resultSpace = Hello.evolve(emptySpace)
+    resultSpace(0, 0) should be(false)
+  }
+
+  "1 cell in 1-cell space" should "die" in {
+    val oneCellSpaceX1Y1 = (x: Int, y: Int) => x == 1 && y == 1
+    val resultSpace = Hello.evolve(oneCellSpaceX1Y1)
+    resultSpace(0, 0) should be(false)
+  }
+
+  "cell with 2 neighbours" should "stay alive" in {
+    val initial = (x: Int, y: Int) => (x, y) match {
+      case (0, 1) => true
+      case (1, 1) => true
+      case (1, 2) => true
+      case _ => false
+    }
+    val result = Hello.evolve(initial)
+    result(1, 1) should be(true)
+  }
+
+  "cell surrounded by crowd" should "die" in {
+    val initial = (x: Int, y: Int) => (x, y) match {
+      case _ => true
+    }
+    val result = Hello.evolve(initial)
+    result(1, 1) should be(false)
+  }
+
+  "dead cell with surrounded by crowd" should "die" in {
+    val initial = (x: Int, y: Int) => (x, y) match {
+      case (0, 1) => true
+      case (1, 2) => true
+      case (2, 2) => true
+      case _ => false
+    }
+    val result = Hello.evolve(initial)
+    result(1, 1) should be(false)
   }
 }
