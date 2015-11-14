e211d004c5e9113ac1f07bb3f4e7a93e4ad8d976
Nikolay Kushin <nikolay@indoo.rs>
2015-11-14 15:28:47 +0100

    crazy

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -4,4 +4,31 @@ object Hello {
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
   }
+
+  trait Neighbourship
+
+  case object ZeroNeighbours extends Neighbourship
+  case object OneNeighbours extends Neighbourship
+
+  trait State
+  case object Dead extends State
+  case object Alive extends State
+
+  trait Cell
+
+  case object AliveWithoutNeighbours extends Cell
+  case object DeadWithoutNeighbours extends Cell
+  case object AliveWith2Neighbours extends  Cell
+
+  def evolve(cell: Cell): State = cell match {
+    case AliveWithoutNeighbours => Dead
+    case DeadWithoutNeighbours => Dead
+    case AliveWith2Neighbours => Alive
+    case _ => ???
+  }
+
+  def withNeighborship(s: State, n: Neighbourship): Cell = (s, n) match {
+    case (Dead, ZeroNeighbours) => DeadWithoutNeighbours
+    case _ => ???
+  }
 }
