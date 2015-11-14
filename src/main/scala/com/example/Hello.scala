620d11a4ae0917e17fe03335a2f9aac6dab1216a
Nikolay Kushin <nikolay@indoo.rs>
2015-11-14 13:40:03 +0100

    1

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -4,4 +4,11 @@ object Hello {
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
   }
+
+  case class Cell(x: Int, y: Int)
+
+  def evolve(gen: Set[Cell]): Set[Cell] = {
+    Set.empty
+  }
+
 }
--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -1,10 +1,16 @@
+import com.example.Hello
 import org.scalatest._
 
 class HelloSpec extends FlatSpec with Matchers {
   "Hello" should "have tests" in {
-    true should be (true)
+    true should be(true)
   }
+
   it should "add numbers" in {
-    1 + 1 should be (2)
+    1 + 1 should be(2)
+  }
+
+  it should "return empty space for empty space" in {
+    Hello.evolve(Set.empty) should be (Set.empty)
   }
 }
